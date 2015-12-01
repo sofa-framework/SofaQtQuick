42eac5f2587390492893d0f11fbdcdf68a4c1eec
Olivier Carré <ocarre@dhcp18.inrialpes.fr>
2015-12-01 12:26:10 +0100

    FIX: examples

--- applications-dev/plugins/SofaQtQuickGUI/examples/callPythonInQML.py
+++ applications-dev/plugins/SofaQtQuickGUI/examples/callPythonInQML.py
@@ -16,13 +16,13 @@ def createScene(root):
     root.createObject('CGLinearSolver',name='CGLinearSolver', template="GraphScattered", iterations="25", tolerance="1e-005", threshold="1e-005")
 
     p0 = root.createChild('P0')
-    p0.createObject('MechanicalObject',         template="Vec3d", name="M0", position="0 0 0 1 0 0")
+    p0.createObject('MechanicalObject',         template="Vec3d", name="M0", position="0 0 0 1 0 0", showObject="1")
     p0.createObject('UniformMass',              template="Vec3d", name="U0")
     p0.createObject('FixedConstraint',          template="Vec3d", name="F0", indices="0")
     p0.createObject('StiffSpringForceField',    template="Vec3d", name="S0", spring="0 1 100 0.1 1", object1="@./", object2="@./")
 
     p1 = root.createChild('P1')
-    m1 = p1.createObject('MechanicalObject',    template="Vec3d", name="M1", position="2 0 0")
+    m1 = p1.createObject('MechanicalObject',    template="Vec3d", name="M1", position="2 0 0", showObject="1")
     p1.createObject('UniformMass',              template="Vec3d", name="U1")
 
     root.createObject('StiffSpringForceField',  template="Vec3d", name="S0", spring="1 0 10 0.1 1", object1="@./P0", object2="@./P1")
