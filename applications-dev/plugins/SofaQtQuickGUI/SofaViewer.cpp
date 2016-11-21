ec2447d361eb37ff9e69a43d70b14697b477e22f
nesme <nesme@anatoscope.com>
2016-11-21 16:02:20 +0100

    [SofaQtQuickGUI] Fix compilation (reverted from commit 7c690201c57d6af98870787a03d0da5e7362685c)
    WARNING this is not the same thing. Please, be sure of your commit or do it in a branch.

--- applications-dev/plugins/SofaQtQuickGUI/SofaViewer.cpp
+++ applications-dev/plugins/SofaQtQuickGUI/SofaViewer.cpp
@@ -77,7 +77,7 @@ SofaViewer::SofaViewer(QQuickItem* parent) : QQuickFramebufferObject(parent),
     myDrawManipulators(true),
     myDrawSelected(true)
 {
-    setMirroredVertically(true);
+    setMirrorVertically(true);
 
     setFlag(QQuickItem::ItemHasContents);
 
