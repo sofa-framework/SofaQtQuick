51e30b3149950cb64586f0dfec7c57ebfd2a506b
Damien Marchal <damien.marchal@univ-lille1.fr>
2018-05-31 15:01:46 +0200

    [SofaCamera.cpp] QMathExtensions to remove duplicated code.

--- applications-dev/plugins/SofaQtQuickGUI/SofaCamera.cpp
+++ applications-dev/plugins/SofaQtQuickGUI/SofaCamera.cpp
@@ -23,6 +23,9 @@ along with sofaqtquick. If not, see <http://www.gnu.org/licenses/>.
 #include <qmath.h>
 #include <iostream>
 
+#include <helper/QMathExtensions.h>
+using sofaqtquickgui::helper::QMath ;
+
 namespace sofa
 {
 
@@ -86,34 +89,23 @@ void SofaCamera::handleSofaDataChange()
 const QMatrix4x4& SofaCamera::projection() const
 {
     if (!m_baseCamera)
-        return QMatrix4x4();
+        return QMath::Identity<QMatrix4x4>();
     
     double dmat[16];
     m_baseCamera->getProjectionMatrix(dmat);
+    QMath::setMatrixFrom(myProjection, dmat);
 
-    QMatrix4x4 qtproj(dmat[0], dmat[1], dmat[2], dmat[3]
-        , dmat[4], dmat[5], dmat[6], dmat[7]
-        , dmat[8], dmat[9], dmat[10], dmat[11]
-        , dmat[12], dmat[13], dmat[14], dmat[15]);
-
-    myProjection = qtproj;
-        
     return myProjection;
 }
 
 const QMatrix4x4& SofaCamera::view() const
 {
     if (!m_baseCamera)
-        return QMatrix4x4();
+        return QMath::Identity<QMatrix4x4>();
 
     double dmat[16];
     m_baseCamera->getModelViewMatrix(dmat);
-    QMatrix4x4 qtmodel(dmat[0], dmat[1], dmat[2], dmat[3]
-        , dmat[4], dmat[5], dmat[6], dmat[7]
-        , dmat[8], dmat[9], dmat[10], dmat[11]
-        , dmat[12], dmat[13], dmat[14], dmat[15]);
-
-    myView = qtmodel;
+    QMath::setMatrixFrom(myView, dmat);
 
     return myView;
 }
