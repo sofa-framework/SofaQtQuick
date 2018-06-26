bada0ac72742eda9c0f7c88444cab77f95deb8ae
Damien Marchal <damien.marchal@univ-lille1.fr>
2018-06-26 22:36:11 +0200

    [SofaQtQuickGUI] First test non crashing !

--- applications-dev/plugins/SofaQtQuickGUI/SofaQtQuickGUI_test/SofaScene_test.cpp
+++ applications-dev/plugins/SofaQtQuickGUI/SofaQtQuickGUI_test/SofaScene_test.cpp
@@ -1,15 +1,30 @@
 #include "SofaScene_test.h"
+#include <QtTest/QTest>
 
-class Toto : public BaseTest{
-public:
-    void qtTests(){
-        SofaScene_test t;
-        QTest::qExec(&t, {"-platform offscreen"});
+class SofaScene_test : public QObject, public BaseTest
+{
+    Q_OBJECT
+
+private slots:
+    void initTestCase()
+    { qDebug("called before everything else"); }
+
+    void myFirstTest()
+    {
+        SofaScene scene;
+        scene.setSource(QUrl(QString("file://")+QString(SOFAQTQUICK_TEST_DIR)+QString("/test1.xml")));
+        //scene.animate();
+
     }
+    void mySecondTest()
+    { QVERIFY(1 != 2); }
+    void cleanupTestCase()
+    { qDebug("called after myFirstTest and mySecondTest"); }
 };
 
-TEST_F(Toto, qtTests)
+
+TEST_F(SofaScene_test, qtTests)
 {
-    Toto::qtTests();
+    QTest::qExec(this) ;
 }
 
