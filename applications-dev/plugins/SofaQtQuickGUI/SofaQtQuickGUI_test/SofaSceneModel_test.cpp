db2ebf3a2de10be0324c485d32d960f409ece211
Damien Marchal <damien.marchal@univ-lille1.fr>
2018-06-27 02:28:05 +0200

    [SofaQtQuickGUI] Add basic test for SofaSceneMode.cpp

--- applications-dev/plugins/SofaQtQuickGUI/SofaQtQuickGUI_test/SofaSceneModel_test.cpp
+++ applications-dev/plugins/SofaQtQuickGUI/SofaQtQuickGUI_test/SofaSceneModel_test.cpp
@@ -4,31 +4,39 @@ using sofa::helper::testing::BaseTest ;
 #include <SofaQtQuickGUI/SofaScene.h>
 using sofa::qtquick::SofaScene ;
 
-#include <SofaQtQuickGUI/SofaSceneListModel.h>
+#include <SofaQtQuickGUI/Models/SofaSceneListModel.h>
 using sofa::qtquick::SofaSceneListModel ;
 
 #include <QApplication>
 
 #include "modeltest.h"
+#include "SofaSceneModel_test.h"
 
-class SofaSceneModel_test : public BaseTest
+void  SofaSceneModel_qtTests::initTestCase(){
+      m_qmltypes=new SofaQtQuickGUI(nullptr) ;
+}
+void  SofaSceneModel_qtTests::cleanupTestCase(){
+    delete m_qmltypes ;
+}
+
+void  SofaSceneModel_qtTests::testAll(){
+    std::cout << SOFAQTQUICK_TEST_DIR << std::endl ;
+    SofaScene scene;
+    scene.setSource(QUrl(QString("file://")+QString(SOFAQTQUICK_TEST_DIR)+QString("/test_minimal.xml")));
+    int cpt=0;
+    while(!scene.isReady() && cpt < 5){ sleep(1); cpt++; };
+    EXPECT_LT(cpt,5);
+    EXPECT_TRUE(scene.isReady());
+    SofaSceneListModel model;
+    model.setSofaScene(&scene);
+
+    ModelTest modeltest(&model) ;
+
+    QMetaObject::invokeMethod( &modeltest, "runAllTests" );
+}
+
+TEST(SofaSceneModel_test, checkGetComponents)
 {
-public:
-    void checkGetComponents(){
-        std::cout << SOFAQTQUICK_TEST_DIR << std::endl ;
-        SofaScene scene;
-        scene.setSource(QUrl(QString("file://")+QString(SOFAQTQUICK_TEST_DIR)+QString("/test1.xml")));
-
-        SofaSceneListModel model;
-        model.setSofaScene(&scene);
-
-        ModelTest modeltest(&model) ;
-
-        QMetaObject::invokeMethod( &modeltest, "runAllTests" );
-    }
-};
-
-TEST_F(SofaSceneModel_test, checkGetComponents)
-{
-    checkGetComponents() ;
+    SofaSceneModel_qtTests tests;
+    QTest::qExec(&tests,{"-platform offscreen"}) ;
 }
