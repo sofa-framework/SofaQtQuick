623215f9d6113dbe5ed3d0340b64c6e6266577c3
Damien Marchal <damien.marchal@univ-lille1.fr>
2019-02-27 04:52:53 +0100

    [SofaQtQuick] Add a new version of SofaData
    I'm using the different namespace to differentiate the two versions.

--- applications-dev/plugins/SofaQtQuickGUI/src/SofaQtQuickGUI/Bindings/SofaData.cpp
+++ applications-dev/plugins/SofaQtQuickGUI/src/SofaQtQuickGUI/Bindings/SofaData.cpp
@@ -1,21 +1,42 @@
-/*
-Copyright 2015, Anatoscope
+/*********************************************************************
+Copyright 2019, Inria, CNRS, University of Lille
 
-This file is part of sofaqtquick.
+This file is part of runSofa2
 
-sofaqtquick is free software: you can redistribute it and/or modify
+runSofa2 is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
 
-sofaqtquick is distributed in the hope that it will be useful,
+runSofa2 is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
 along with sofaqtquick. If not, see <http://www.gnu.org/licenses/>.
-*/
+*********************************************************************/
+/********************************************************************
+ Contributors:
+    - damien.marchal@univ-lille.fr
+********************************************************************/
+#include "SofaData.h"
+#include "../DataHelper.h"
+
+namespace sofaqtquick::bindings::_sofadata_
+{
+
+SofaData::SofaData(BaseData* self)
+{
+    m_self = self;
+}
+
+QVariant SofaData::value() const
+{
+    return sofaqtquick::helper::createQVariant(m_self);
+}
+
+}
 
 #include <GL/glew.h>
 #include <SofaQtQuickGUI/Bindings/SofaData.h>
--- applications-dev/plugins/SofaQtQuickGUI/src/SofaQtQuickGUI/Bindings/SofaData.h
+++ applications-dev/plugins/SofaQtQuickGUI/src/SofaQtQuickGUI/Bindings/SofaData.h
@@ -1,26 +1,54 @@
-/*
-Copyright 2015, Anatoscope
+/*********************************************************************
+Copyright 2019, Inria, CNRS, University of Lille
 
-This file is part of sofaqtquick.
+This file is part of runSofa2
 
-sofaqtquick is free software: you can redistribute it and/or modify
+runSofa2 is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
 
-sofaqtquick is distributed in the hope that it will be useful,
+runSofa2 is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
 along with sofaqtquick. If not, see <http://www.gnu.org/licenses/>.
-*/
+*********************************************************************/
+/********************************************************************
+ Contributors:
+    - damien.marchal@univ-lille.fr
+********************************************************************/
+#pragma once
 
-#ifndef SOFA_DATA_H
-#define SOFA_DATA_H
-
+#include <QObject>
 #include <SofaQtQuickGUI/config.h>
+#include <sofa/core/objectmodel/BaseData.h>
+
+namespace sofaqtquick::bindings
+{
+    namespace _sofadata_
+    {
+        using sofa::core::objectmodel::BaseData;
+
+        class SofaData : public QObject
+        {
+            Q_OBJECT
+            Q_PROPERTY(QVariant value READ value)
+
+        public:
+            SofaData(BaseData* self);
+
+            QVariant value() const;
+            void setValue(const QVariant& value);
+
+        private:
+            BaseData* m_self;
+        };
+    }
+    using _sofadata_::SofaData;
+}
 
 #include <sofa/simulation/Simulation.h>
 #include <QVariantMap>
@@ -68,4 +96,3 @@ private:
 
 }
 
-#endif // SOFA_DATA_H
