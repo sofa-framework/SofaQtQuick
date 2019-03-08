1d907b45314fa46c03caa8afec251b23097e7079
Damien Marchal <damien.marchal@univ-lille1.fr>
2019-03-08 18:29:38 +0100

    [runSofa2] ADD forward from qt message system into sofa's.

--- applications-dev/projects/runSofa2/Main.cpp
+++ applications-dev/projects/runSofa2/Main.cpp
@@ -17,13 +17,38 @@ You should have received a copy of the GNU General Public License
 along with sofaqtquick. If not, see <http://www.gnu.org/licenses/>.
 */
 
+#include <sofa/helper/logging/Messaging.h>
 #include <SofaQtQuickGUI/SofaApplication.h>
 #include <QtWebView/QtWebView>
 #include <runSofa2/runSofa2.h>
 #include <QQuickStyle>
 
+void myMessageOutput(QtMsgType type, const QMessageLogContext &context, const QString &msg)
+{
+    QByteArray localMsg = msg.toLocal8Bit();
+    switch (type) {
+    case QtDebugMsg:
+        dmsg_info_withfile("runSofa2", context.file, context.line) << localMsg.constData();
+        break;
+    case QtInfoMsg:
+        msg_info_withfile("runSofa2", context.file, context.line) << localMsg.constData();
+        break;
+    case QtWarningMsg:
+        msg_warning_withfile("runSofa2", context.file, context.line) << localMsg.constData();
+        break;
+    case QtCriticalMsg:
+        msg_error_withfile("runSofa2", context.file, context.line) << localMsg.constData();
+        break;
+    case QtFatalMsg:
+        msg_error_withfile("runSofa2", context.file, context.line) << localMsg.constData();
+        //abort();
+    }
+}
+
 int main(int argc, char **argv)
 {
+    qInstallMessageHandler(myMessageOutput); // Install the handler
+
     // IMPORTANT NOTE: this function MUST be call before QApplication creation in order to be able to load a SofaScene containing calls to OpenGL functions (e.g. containing OglModel)
     sofa::qtquick::SofaApplication::Initialization();
 
@@ -36,6 +61,7 @@ int main(int argc, char **argv)
     app.setOrganizationName("Sofa Consortium");
     app.setApplicationName("runSofa2");
     app.setApplicationVersion("v1.0");
+
 
     // common settings for most sofaqtquick applications
     if(!sofa::qtquick::SofaApplication::DefaultMain(app, applicationEngine, "qrc:/qml/Main.qml"))
